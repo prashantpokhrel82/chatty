e4b3653a01807c2631a6b167717038df5438988c
Prashant <prashantpokhrel15@gmail.com>
2023-01-15 14:44:47 +1100

    chat feed setup and styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
+      userName={process.env.REACT_APP_CHAT_ENGINE_USER_NAME}
+      userSecret={process.env.REACT_APP_CHAT_ENGINE_USER_SECRET}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
 }
 
 export default App;
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ChatFeed = (props) => {
+  console.log(props);
+
+  return <div>ChatFeed ChatFeed</div>;
+};
+
+export default ChatFeed;
